8fa4be7fd0ef17f15f5eb8193145391bf17f90e3
Sacha Greif <sacha357@gmail.com>
2013-05-09 13:28:22 -0700

    using iframe

--- client/twitterlist.js
+++ client/twitterlist.js
@@ -3,18 +3,3 @@ Meteor.subscribe('allUsers');
 Template.content.users = function () {
 	return Meteor.users.find();
 };
-
-Template.user.rendered = function(){
-
-! function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-        p = /^http:/.test(d.location) ? 'http' : 'https';
-    if (!d.getElementById(id)) {
-        js = d.createElement(s);
-        js.id = id;
-        js.src = p + '://platform.twitter.com/widgets.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }
-}(document, 'script', 'twitter-wjs');
-
-}
