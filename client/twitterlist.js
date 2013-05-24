c712a9a0045867f88d83301241a9ea17cd2390e9
Sacha Greif <sacha357@gmail.com>
2013-05-24 09:23:48 +0900

    updated meteor and simplified code a lot

--- client/twitterlist.js
+++ client/twitterlist.js
@@ -1,17 +1,3 @@
-Session.set('usersLoaded', false);
-
-Meteor.subscribe('allUsers', function(){
-	Session.set('usersLoaded', true);
-});
-
 Template.content.users = function () {
 	return Meteor.users.find();
 };
-
-Template.user.rendered = function(){
-	// if(Meteor.user() && !Meteor.loggingIn())
-		// console.log(this.data.services.twitter.screenName)
-}
-Template.user.usersReady = function(){
-	return Session.get('usersLoaded');
-}
